9ba223e0d58940a3370e0c50fccd0ba8f1a8f557
Rafael da Silva Nogueira <rsnogueira@sfiec.org.br>
2019-11-13 17:09:03 -0300

    Subindo app no heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,9 +13,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import psycopg2.extensions
 import django_heroku
-django_heroku.settings(locals())
 
-TEST_RUNNER = 'django_heroku.HerokuDiscoverRunner'
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
