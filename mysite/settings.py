775debc5072757f60106a57949a85b455f2648c0
Rafael da Silva Nogueira <rsnogueira@sfiec.org.br>
2019-11-14 08:34:17 -0300

    Arquivos configuração do heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ import os
 import psycopg2.extensions
 import django_heroku
 
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -41,6 +41,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+django_heroku.settings(locals())
 
 # Application definition
 
